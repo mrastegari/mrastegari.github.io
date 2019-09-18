5599b422ec283365657bf0e2d4a674c817b31d68
mohammadr <mohammadr@changeme2014-07.local>
2019-09-18 13:14:44 -0700

    first version of homepage uploaded

---
+++ my_homepage/news_old.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>
+Jonghyun Choi @ ECE UMD
+</title>
+<LINK media = all href="default.css" type = text/css rel=StyleSheet>
+<!--STYLE type=text/css media=all>img{
+    PADDING-RIGHT: 0px; 
+	PADDING-LEFT: 0px; 
+	PADDING-BOTTOM: 0px; 
+	PADDING-TOP: 50px;
+	FLOAT: right;
+}
+</STYLE> 
+-->
+
+<body>
+
+<?php include("navigation.php"); ?>
+
+<h2> Past News </h2>
+<li>[2012.4.24] We got two papers accepted in CVPR Workshop in Biometrics </li>
+<li>[2012.4.23~27] Attend SPIE DSS conference 2012, Baltimore, MD</li>
+<li>[2012.1.9~14] Attended WACV 2012, Breckenridge, CO</li>
+<li>[2011.12.14] PDF Excerpt script is download available <a href="./sw/pdfexcerpt.zip">here</a></li>
+<li>[2011.12.3] Our paper on <a  href="http://umiacs.umd.edu/~jhchoi/pub.php">face recognition for thermal imagery</a> will be presented in SPIE Biometrics conference 2012 (April) as an oral presentation</li>
+<li>[2011.10.31] Our paper on <a  href="http://umiacs.umd.edu/~jhchoi/pub.php">face recognition with a new feature</a> will be presented in IEEE WACV 2012 (Jan.) as a full oral [Acceptance Rate: 8.5%]</li>
+<li>[2011.10.29] Our paper on <a href="http://umiacs.umd.edu/~jhchoi/pub.php">face recognition with many features</a> is accepted to IEEE Trans. on IP</li>
+<li>[2011.7.6] Contribute to update <a href="http://www.vlfeat.org/sandbox/index.html" target="_blank">VL_FEAT</a> compile script (<a href="https://github.com/vlfeat/vlfeat/blob/master/toolbox/vl_compile.m">vl_compile.m (See Authors)</a>) for different platform. Now it's part of <a href="http://www.vlfeat.org/sandbox/download.html" target="_blank">VL_FEAT Ver. 0.9.12</a> release. Thanks to Andrea Vedaldi </li>
+<li>[2011.6] Attending CVPR 2011</li>
+<li>[2011.5] I am doing an internship at <a href="http://www.arl.army.mil/www/default.cfm">U.S. Army Research Laboratory</a> during summer 2011.</li>
+<li>[2011.4] I am going to participate in CVPR 2011 in Colorado Springs, CO. (19 June ~ 25 June)</li>
+
+
+<?php include("footer.php"); ?>
+
+</body>
+
+</html>
---
+++ my_homepage/talks.php
@@ -0,0 +1,5 @@
+<div id="container">
+<li> Graduate-student Research Interaction Day (GRID), University of Maryland, April 2013</li>
+<li> 1st KSEA-UMD Academic Symposium, University of Maryland, March 2013 <b>[Best Presentation Award]</b></li>
+<li> Computer Vision Student Seminar, University of Maryland, May 2012</li>
+</div>
