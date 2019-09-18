725b7e90a2c999206668eec1db4d0b6a2ea144ec
Mohammad Rastegari <mohammadrastegari@MohammasMacBook.corp.xnor.ai>
2019-09-18 16:35:30 -0700

    changes

--- invtalk.php
+++
@@ -1,8 +0,0 @@
-<div id="container">
-<li> Oxford University at Oxford, UK.
-     <br>Rich and Efficient Visual Data Representation</br> 
-<li> University of Illinois at Urbana Champaign, IL.
-     <br>On Attribute Discovery and its Applications</br> 
-<li> University of Texas at Austin.
-     <br>On Attribute Discovery and its Applications</br> 
-</div>
--- services.php
+++
@@ -1,8 +0,0 @@
-<div id="container">
-<li> Reviewer of the following journals and conferences </li>
-	<ol> - IEEE Transactions on Information Forensics and Security, 2013</ol>
-	<ol> - IEEE Transactions on Aerospace and Electronics Systems, 2012-2013</ol>
-	<ol> - Computer Vision and Image Understanding (CVIU), 2012 </ol>
-<li> Vice Chair of Academic Affair, Korean-American Scientist and Engineer's Association (KSEA) Washington Metro Chapter, 2013 </li>
-<li> Publication Chair, Korea-Japan Joint Workshop on Frontiers of Computer Vision (FCV), 2007 </li>
-</div>
